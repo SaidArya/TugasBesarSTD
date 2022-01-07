1270952bf567bd612532ca2f348be05d53dbe398
SaidArya <saidarya@student.telkomuniversity.ac.id>
2022-01-07 18:38:51 +0800

    Added structs

--- src/event.h
+++ src/event.h
@@ -1,5 +1,37 @@
 #ifndef EVENT_H
 #define EVENT_H
+#include <iostream>
+using namespace std;
 
+typedef struct elementEvent *addressEvent;
+typedef struct elementPeserta *addressPeserta;
+
+struct event {
+    string namaEvent, jenisEvent;
+    tanggal tanggalEvent;
+    int quota, nPeserta;
+};
+struct peserta {
+    string namaPeserta, emailPeserta, jenisPeserta;
+    int noPeserta, noTelepon, noTempatDuduk;
+};
+struct elementEvent {
+    event info;
+    addressEvent next, prev;
+    addressPeserta nextPeserta;
+};
+struct elementPeserta {
+    peserta info;
+    addressPeserta next, prev;
+};
+struct tanggal {
+    int hari, tahun;
+    string bulan;
+};
+struct list {
+    addressEvent first;
+};
+
+void createList(list &L);
 
 #endif /* EVENT_H */
